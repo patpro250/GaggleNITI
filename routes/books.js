226fe382ab289b919004d475c635813875823bbe
Terry <madeinterry@gmx.us>
2025-01-27 20:28:10 -0800

    Add: delete and update book features

--- routes/books.js
+++ routes/books.js
@@ -28,6 +28,25 @@ router.post("/", async (req, res) => {
   res.status(201).send("New book is created");
 });
 
+router.put("/:id", async (req, res) => {
+  const {error} = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const book = await prisma.book.findUnique({where: {id: req.params.id}});
+  if (!book) return res.status(404).send(`Book with ID: ${req.params.id} doen't exist`);
+
+  book = await prisma.book.update({where: {id: req.params.id}, data: req.body});
+  res.status(200).send("Book successfully updated!");
+});
+
+router.delete("/:id", async (req, res) => {
+    const book = await prisma.book.findUnique({where: {id: req.params.id}});
+    if (!book) return res.status(404).send(`Book with ID: ${req.params.id} not found`);
+
+    book = await prisma.book.delete({where: {id: req.params.id}});
+    res.status(200).send("Book deleted successfully!");
+});
+
 function validate(book) {
   const schema = Joi.object({
     title: Joi.string().required().min(3),
