f731bbb549173b61e4667aae4f0300e3ec383733
Terry <madeinterry@gmx.us>
2025-02-09 04:40:54 +0200

    Refactor: Let the firstName be followed by lastName and email after in auth.js

--- middleware/auth/librarian.js
+++ middleware/auth/librarian.js
@@ -3,7 +3,6 @@ const prisma = new PrismaClient();
 
 module.exports = async function (req, res, next) {
     let isLibrarian = await prisma.librarian.findFirst({where: {librarianId: req.user.librarianId}});
-    console.log(req.user);
     if (!isLibrarian) return res.status(400).send('This action can be done by Librarian only.');
     next();
 }
