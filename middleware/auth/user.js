c1456052a3cea60588cda4b701fa734b9cc8c79f
Terry <madeinterry@gmx.us>
2025-02-14 17:42:48 +0200

    Modify the add book route and remove the bug in the application

--- middleware/auth/user.js
+++ middleware/auth/user.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = function (req, res, next) {
-    if (req.path.startsWith('/auth')) return next();
+    if (req.path.startsWith('/auth') || (req.path === '/institutions' && req.method === 'POST')) return next();
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).send('Access denied. No token provided');
 
--- routes/books.js
+++ routes/books.js
@@ -8,25 +8,22 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
-  let { cursor, limit, q, sort } = req.query;
+  let { cursor, limit, q, sort, language } = req.query;
   limit = parseInt(limit) || 10;
 
   if (limit > 50) limit = 50;
   const orderBy = sort === 'asc' ? { createdAt: 'asc' } : { createdAt: 'desc' };
 
-  const whereClause = q
-    ? {
-      AND: [
-        { institutionId: req.user.institutionId },
-        {
-          OR: [
-            { title: { contains: q, mode: "insensitive" } },
-            { author: { contains: q, mode: "insensitive" } }
-          ]
-        },
-      ],
-    }
-    : { institutionId: req.user.institutionId };
+  const whereClause = {
+    institutionId: req.user?.institutionId,
+    ...(q && {
+      OR: [
+        { title: { contains: q, mode: "insensitive" } },
+        { author: { contains: q, mode: "insensitive" } }
+      ]
+    }),
+    ...(language && { language: { equals: language, mode: "insensitive" } })
+  };
 
 
   const books = await prisma.book.findMany({
@@ -41,23 +38,34 @@ router.get("/", async (req, res) => {
   res.status(200).send({ nextCursor, books });
 });
 
+router.get("/", async (req, res) => {
+  
+});
+
 router.post("/", librarianAuth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const exists = await prisma.book.findFirst({ where: { institutionId: req.body.institutionId } });
-  if (exists) return res.status(400).send(`'${req.body.title}' already exists`);
-
-  const institution = await prisma.institution.findUnique({
+  const exists = await prisma.book.findFirst({
     where: {
-      id: req.body.institutionId
+      AND: [
+        { institutionId: req.user.institutionId },
+        { edition: {  equals: req.body.edition  } }
+      ]
     }
   });
+  if (exists) return res.status(400).send(`'${req.body.title}' edition: '${req.body.edition}' already exists in library collection`);
+
+  const institution = await prisma.institution.findUnique({
+    where: { id: req.user.institutionId }
+  });
+
+  req.body.institutionId = req.user.institutionId
 
   if (!institution) return res.status(404).send("Institution not found");
-  await prisma.book.create({ data: req.body });
+  let book = await prisma.book.create({ data: req.body });
 
-  res.status(201).send("New book is created");
+  res.status(201).send(`${book.title} is added in Library collection`);
 });
 
 router.put("/:id", async (req, res) => {
@@ -94,8 +102,7 @@ function validate(book) {
     shelfLocation: Joi.string().required(),
     callNo: Joi.string().min(3).max(20),
     barCode: Joi.string().max(15),
-    ddcCode: Joi.string().max(15),
-    institutionId: Joi.string().required(),
+    ddcCode: Joi.string().max(15)
   });
   return schema.validate(book);
 }
