410296bd8c3b1158846e350ee1d92bc19f8f89e6
iraguha patrick <patrickiraguha95@gmail.com>
2025-05-23 18:18:17 +0200

    update payment

--- routes/payments.js
+++ routes/payments.js
@@ -10,14 +10,17 @@ router.get("/", async (req, res) => {
 
   const { status } = req.query;
 
-  if (status && !['PENDING', 'SUCCESS', 'FAILED'].includes(status.toUpperCase())) {
-    status = 'PENDING';
+  if (
+    status &&
+    !["PENDING", "SUCCESS", "FAILED"].includes(status.toUpperCase())
+  ) {
+    status = "PENDING";
   }
 
   const payments = await prisma.payment.findMany({
     where: {
       institutionId: institution,
-      ...(status && { status: status.toUpperCase() })
+      ...(status && { status: status.toUpperCase() }),
     },
   });
 
@@ -29,13 +32,15 @@ router.get("/all", permission(["SYSTEM_ADMIN"]), async (req, res) => {
   res.status(200).send(payments);
 });
 
-router.post('/momo', async (req, res) => {
+router.post("/momo", async (req, res) => {
   const { institutionId } = req.user;
   const { error } = validatePaymentRequest(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const plan = await prisma.pricingPlan.findFirst({ where: { id: req.body.planId } });
-  if (!plan || amount < plan.price) {
+  const plan = await prisma.pricingPlan.findFirst({
+    where: { id: req.body.planId },
+  });
+  if (!plan || req.body.amount < plan.price) {
     return res.status(400).send("Invalid amount or plan.");
   }
 
@@ -44,72 +49,88 @@ router.post('/momo', async (req, res) => {
       institutionId,
       planId: req.body.planId,
       amount: req.body.amount,
-      currency: 'RWF',
-      status: 'PENDING',
-      method: 'MOBILE_MONEY',
-    }
+      currency: "RWF",
+      status: "PENDING",
+      method: "MOBILE_MONEY",
+    },
   });
 
-  res.status(200).send('Payment submitted. Awaiting confirmation.');
-
+  res.status(200).send("Payment submitted. Awaiting confirmation.");
 });
 
-router.patch('/confirm/:paymentId', permission(["SYSTEM_ADMIN"]), async (req, res) => {
-  const paymentId = req.params.paymentId;
+router.patch(
+  "/confirm/:paymentId",
+  permission(["SYSTEM_ADMIN"]),
+  async (req, res) => {
+    const paymentId = req.params.paymentId;
 
-  const payment = await prisma.payment.findFirst({ where: { id: paymentId, status: { not: 'PENDING' } } });
-  if (!payment) return res.status(404).send(`Payment not found or already confirmed!`);
+    const payment = await prisma.payment.findFirst({
+      where: { id: paymentId, status: { not: "PENDING" } },
+    });
+    if (!payment)
+      return res.status(404).send(`Payment not found or already confirmed!`);
 
-  const now = new Date();
-  var expiresAt;
+    const now = new Date();
+    var expiresAt;
 
-  const activePurchase = await prisma.purchase.findFirst({
-    where: {
-      institutionId: payment.institutionId,
-      expiresAt: { gte: now },
-    }
-  });
-
-  if (activePurchase) {
-    const timeLeft = activePurchase.expiresAt - now;
-    const newExpiryDate = new Date(now.getTime() + timeLeft + (30 * 24 * 60 * 60 * 1000));
-    expiresAt = newExpiryDate;
-  } else {
-    expiresAt = now + (30 * 24 * 60 * 60 * 1000);
-  }
-
-  const institution = await prisma.institution.findFirst({ where: { id: payment.institutionId }, select: { name: true } });
-  const paymentCode = generateConfirmationCode(institution.name);
-
-  if (!paymentCode) return res.status(400).send(`Failed to generate payment code!`);
-
-  await prisma.$transaction([
-    prisma.payment.update({ where: { id: paymentId }, data: { status: 'SUCCESS', confirmationCode: paymentCode } }),
-    prisma.purchase.create({
-      data: {
+    const activePurchase = await prisma.purchase.findFirst({
+      where: {
         institutionId: payment.institutionId,
-        planId: payment.planId,
-        amount: payment.amount,
-        currency: payment.currency,
-        expiresAt,
-      }
-    })
-  ]);
+        expiresAt: { gte: now },
+      },
+    });
 
-  res.status(200).send(paymentCode);
-});
+    if (activePurchase) {
+      const timeLeft = activePurchase.expiresAt - now;
+      const newExpiryDate = new Date(
+        now.getTime() + timeLeft + 30 * 24 * 60 * 60 * 1000
+      );
+      expiresAt = newExpiryDate;
+    } else {
+      expiresAt = now + 30 * 24 * 60 * 60 * 1000;
+    }
+
+    const institution = await prisma.institution.findFirst({
+      where: { id: payment.institutionId },
+      select: { name: true },
+    });
+    const paymentCode = generateConfirmationCode(institution.name);
+
+    if (!paymentCode)
+      return res.status(400).send(`Failed to generate payment code!`);
+
+    await prisma.$transaction([
+      prisma.payment.update({
+        where: { id: paymentId },
+        data: { status: "SUCCESS", confirmationCode: paymentCode },
+      }),
+      prisma.purchase.create({
+        data: {
+          institutionId: payment.institutionId,
+          planId: payment.planId,
+          amount: payment.amount,
+          currency: payment.currency,
+          expiresAt,
+        },
+      }),
+    ]);
+
+    res.status(200).send(paymentCode);
+  }
+);
 
 function validatePaymentRequest(request) {
   const schema = Joi.object({
     amount: Joi.number().required(),
     phoneNumber: Joi.string().required(),
-    planId: Joi.string().uuid().required()
+    planId: Joi.string().uuid().required(),
+    duration: Joi.number().required(),
   });
   return schema.validate(request);
 }
 
 function generateConfirmationCode(institutionName) {
-  const cleaned = institutionName.trim().replace(/\s+/g, '');
+  const cleaned = institutionName.trim().replace(/\s+/g, "");
 
   if (cleaned.length < 2) {
     throw new Error("Institution name must be at least 2 characters long.");
