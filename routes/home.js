c190580692a4aea0882742ce664aad607a1bc900
Terry <madeinterry@gmx.us>
2025-06-08 10:06:05 +0200

    Update dependencies and remove unused column in migration

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,11 @@ router.get('/', (req, res) => {
     res.status(200).send({
         message: "Welcome to the API",
         version: "1.0.0",
-        producedBy: "Gaggle NITI Group"
+        producedBy: "Gaggle NITI Group",
+        contact: {
+            name: "Gaggle NITI Support",
+            email: "nzabonerwanayo@gmail.com"
+        }
     })
 });
 
