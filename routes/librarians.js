2979bb886dc49e056e9a03e1b98a14ea078fcd30
Terry <madeinterry@gmx.us>
2025-06-14 13:01:02 +0200

    Refactor librarian creation to remove institutionCode and set institutionId

--- routes/librarians.js
+++ routes/librarians.js
@@ -198,6 +198,8 @@ router.post("/create", async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   req.body.password = await bcrypt.hash(req.body.password, salt);
   req.body.status = "PENDING";
+  delete req.body.institutionCode;
+  req.body.institutionId = institution.id;
 
   await prisma.librarian.create({ data: req.body });
 
