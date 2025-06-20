0a393d0ae9889b7e4bec839c40294169782c074c
iraguha patrick <patrickiraguha95@gmail.com>
2025-06-20 17:36:49 +0200

    Format app.js for consistency and clarity

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const http = require("http");
 
 const server = http.createServer(app);
 
-server.keepAliveTimeout = 65000; 
-server.headersTimeout = 66000; 
+server.keepAliveTimeout = 65000;
+server.headersTimeout = 66000;
 
+app.set("trust proxy", 1);
 require("./startup/logging")();
 require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
+
 // require("./startup/jobs")();
 
 const PORT = process.env.PORT || 4000;
